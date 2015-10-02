34d3eebb5c21eb7a96989a84b9a9ac1a8c471389
stefanschweng <stefan.schweng@icloud.com>
2015-10-02 20:46:13 +0200

    Change4

--- Rechner1/ViewController.swift
+++ Rechner1/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ergebnisLabel: UILabel!
     @IBOutlet weak var zahl1Field: UITextField!
     @IBOutlet weak var zahl2Field: UITextField!
+    @IBOutlet weak var hello: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
